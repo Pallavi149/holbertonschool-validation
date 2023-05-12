d740e911d6b41ec45100025b324d11a13618d1e7
pallavi149 <5811@holbertonstudents.com>
2023-05-12 02:28:16 +0000

    Added tests

--- module2_task2/main_test.go
+++ module2_task2/main_test.go
@@ -30,7 +30,29 @@ func Test_HelloHandler(t *testing.T) {
       responseCode: 200,
       body:         "Hello Rosalind Franklin!",
     },
-    // INSERT MORE TESTS HERE
+		{
+    
+			name:         "Eps Rarima",
+			queryString:  "name=",
+			responseCode: 400,
+		},
+		{
+			name:         "Jonathan Clus",
+			queryString:  "name=You are the best!",
+			responseCode: 200,
+			body:         "Hello You are the best!",
+		},
+		{
+			name:         "Jimmy Recard",
+			queryString:  "",
+			responseCode: 200,
+			body:         "Hello there!",
+		},
+		{
+			name:         "",
+			queryString:  "name=",
+			responseCode: 400,
+		},
   }
   for _, tt := range tests {
     t.Run(tt.name, func(t *testing.T) {
