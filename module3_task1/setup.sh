2df42dc729de8e2a73f4a1af9e750dafbd51b7d8
Pallavi149 <113876587+Pallavi149@users.noreply.github.com>
2023-05-16 22:33:02 +1000

    added sudo for for commands

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -2,4 +2,5 @@
 wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.tar.gz
 tar -zxvf hugo_extended_0.84.0_Linux-64bit.tar.gz
 sudo mv hugo /usr/local/bin/
-apt-get update && apt-get install -y hugo make
+sudo apt-get update 
+sudo apt-get install -y hugo make
