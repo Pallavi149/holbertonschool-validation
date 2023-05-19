740ddf5ba83183599fc44b1e2f6bf3be50e284c2
pallavi149 <5811@holbertonstudents.com>
2023-05-18 22:25:55 -0700

    Adding necessary files

---
+++ module3_task2/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.tar.gz
+tar -zxvf hugo_extended_0.84.0_Linux-64bit.tar.gz
+sudo mv hugo /usr/local/bin/
+sudo apt-get update 
+sudo apt-get install -y hugo make
+npm install -g markdownlint-cli
