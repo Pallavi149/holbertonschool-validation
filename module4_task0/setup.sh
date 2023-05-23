14ad63fbdeaac6eed29e3eb9f55489eaf3be2697
pallavi149 <5811@holbertonstudents.com>
2023-05-23 06:23:17 -0700

    fixed workflow

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -3,5 +3,5 @@ wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.
 tar -zxvf hugo_extended_0.84.0_Linux-64bit.tar.gz
 sudo mv hugo /usr/local/bin/
 sudo apt-get update 
-sudo apt-get install -y hugo make
+sudo apt-get install -y make
 sudo npm install -g markdownlint-cli
