6c81539438baa155804577c7502828a3fced855a
Pallavi149 <113876587+Pallavi149@users.noreply.github.com>
2023-05-26 21:25:40 +1000

    installing wget

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo apt-get update 
+sudo apt-get install make wget
 wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.tar.gz
 tar -zxvf hugo_extended_0.84.0_Linux-64bit.tar.gz
 sudo mv hugo /usr/local/bin/
