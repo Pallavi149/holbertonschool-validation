cb5bb82912be59af8d9827d3051dcb1ecb796ae2
Pallavi149 <113876587+Pallavi149@users.noreply.github.com>
2023-05-26 21:36:27 +1000

    Update setup.sh

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -4,4 +4,8 @@ sudo apt-get install -y make wget
 wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.tar.gz
 tar -zxvf hugo_extended_0.84.0_Linux-64bit.tar.gz
 sudo mv hugo /usr/local/bin/
+
+curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
+sudo apt -y install nodejs
+
 sudo npm install -g markdownlint-cli
